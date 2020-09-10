b8df4d8a3b58a1db4c3bfb14e61131bac34410d7
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-09-10 11:22:42 +0100

    remove old unused files

--- extract_example.py
+++
@@ -1,8 +0,0 @@
-output = []
-
-with open("paper.md") as f:
-    for part in f.read().split("```python")[1:]:
-        output.append(part.split("```")[0])
-
-with open("example.py", "w") as f:
-    f.write("\n\n".join(output))
